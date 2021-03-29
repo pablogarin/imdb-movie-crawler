4027852d3eb4123b5dc93a3e70b122fdd553b04b
pablogarin <pablo.garin@hotmail.com>
2021-03-28 23:57:59 -0300

    Feat: indexes are now configurable

--- imdb_movie_api/imdb_reader/imdb_reader.py
+++ imdb_movie_api/imdb_reader/imdb_reader.py
@@ -13,58 +13,51 @@ class IMBDReader(object):
     movie_list = []
     data_table = {}
     search_params = Trie()
+    indexes = [
+        "title",
+        "director",
+        "stars",
+        "genres",
+        "year"
+    ]
     MAX_PAGES = 10
 
     def __init__(self):
         print("Fetching movie information...")
         self.movie_list = []
+        # Fetch all movies and parse them
         for page in range(self.MAX_PAGES):
             start = 0
             if page > 0:
                 start = 100*page+1
             self.movie_list += self._get_movies(start)
+        # Index all results for quick lookups
         for movie in self.movie_list:
-            # title search
-            title = movie.title
-            for word in title.strip().lower().split(" "):
-                word = normalize(word)
-                final_node = self.search_params.add(word)
-                if final_node is not None:
-                    final_node.keys.add(title)
-            self.data_table[title] = {movie}
-            # directors hash table
-            director = movie.director
-            if director not in self.data_table:
-                self.data_table[director] = set()
-            self.data_table[director].add(movie)
-            for word in director.lower().split(' '):
-                word = normalize(word)
-                final_node = self.search_params.add(word)
-                if final_node is not None:
-                    final_node.keys.add(director)
-            # stars hash table
-            stars = movie.stars
-            for star in stars:
-                if star not in self.data_table:
-                    self.data_table[star] = set()
-                self.data_table[star].add(movie)
-                for word in star.lower().split(' '):
-                    word = normalize(word)
-                    final_node = self.search_params.add(word)
-                    if final_node is not None:
-                        final_node.keys.add(star)
-            # genre hash table
-            genres = movie.genres
-            for genre in genres:
-                if genre not in self.data_table:
-                    self.data_table[genre] = set()
-                self.data_table[genre].add(movie)
-                for word in genre.lower().split(' '):
-                    word = normalize(word)
-                    final_node = self.search_params.add(word)
-                    if final_node is not None:
-                        final_node.keys.add(genre)
+            dict_movie = dict(movie)
+            for index in self.indexes:
+                if index not in dict_movie:
+                    continue
+                keyword = dict_movie[index]
+                if type(keyword) == str:
+                    self._setup_keyword(keyword, movie)
+                elif type(keyword) == list:
+                    for kw in keyword:
+                        self._setup_keyword(kw, movie)
         print("Movies dataset loaded.")
+
+    def _setup_keyword(self, keyword, movie):
+        # If the keyword is not in the lookup table, add it
+        if keyword not in self.data_table:
+            self.data_table[keyword] = set()
+        self.data_table[keyword].add(movie)
+        # add each word to the Trie data structure
+        for word in keyword.strip().lower().split(" "):
+            final_node = self.search_params.add(normalize(word))
+            if final_node is not None:
+                """ If we reached the final node, add the
+                full keyword in the node key set in order
+                to match it with the lookup table"""
+                final_node.keys.add(keyword)
 
     def _get_movies(self, start=0, count=100):
         try:
